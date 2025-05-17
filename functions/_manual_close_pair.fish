51a6bfd94451857f980175b6f98db00bfdab51d5
Bagohart <bagohart@gmx.de>
2025-05-17 22:25:48 +0200

    fix plugin for multi-line input

--- functions/_manual_close_pair.fish
+++ functions/_manual_close_pair.fish
@@ -1,6 +1,6 @@
 function _manual_close_pair --description 'If there is an opening bracket or a quote before the cursor, insert a closing bracket or a matching quote after the cursor.'
     set char_before_cursor $(string sub --start=-1 $(commandline --cut-at-cursor))
-    switch $char_before_cursor
+    switch $char_before_cursor[-1]
         case '('
             set closing_char ')'
         case '['
